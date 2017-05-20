4ee1de8f861215724a70a77082e94ca5258753df
defuyun <carlliu2012@gmail.com>
2017-05-21 00:13:22 +1000

    added a move action

--- Engine/doge/action.cpp
+++ Engine/doge/action.cpp
@@ -7,3 +7,30 @@ void doge::action::addObj(const std::shared_ptr<doge::object> & obj) {
 std::unordered_set<std::shared_ptr<doge::object>> & doge::action::getBoundObjs() {
 	return this->boundObjs;
 }
+
+void doge::move::execute(std::unique_ptr<doge::mainEngineControl> & mec, std::shared_ptr<doge::object> & obj) {
+	if (mec->isKeyPressed(_front)) {
+		obj->setPos(obj->getPos() + obj->getFront() * this->_moveSpeed);
+	} else if (mec->isKeyPressed(_back)) {
+		obj->setPos(obj->getPos() - obj->getFront() * this->_moveSpeed);
+	} else if (mec->isKeyPressed(_right)) {
+		obj->setPos(obj->getPos() + glm::normalize(glm::cross(obj->getFront(), obj->getUp()) * this->_moveSpeed));
+	} else {
+		obj->setPos(obj->getPos() - glm::normalize(glm::cross(obj->getFront(), obj->getUp()) * this->_moveSpeed));
+	}
+}
+
+bool doge::move::check(std::unique_ptr<mainEngineControl> & mec, const std::shared_ptr<doge::object> & obj) const {
+	return mec->isKeyPressed(_front) || mec->isKeyPressed(_back) || mec->isKeyPressed(_left) || mec->isKeyPressed(_right);
+}
+
+void doge::move::setMoveSpeed(float moveSpeed) {
+	this->_moveSpeed = moveSpeed;
+}
+
+void doge::move::setKeyBinding(int front, int back, int left, int right) {
+	this->_front = front;
+	this->_back = back;
+	this->_left = left;
+	this->_right = right;
+}
--- Engine/doge/action.h
+++ Engine/doge/action.h
@@ -12,4 +12,20 @@ namespace doge {
 		virtual void addObj(const std::shared_ptr<doge::object> & obj);
 		std::unordered_set<std::shared_ptr<doge::object>> & getBoundObjs();
 	};
+
+	class move : public action {
+	private:
+		float _moveSpeed;
+		int _front, _back, _left, _right;
+	public:
+		virtual void execute(std::unique_ptr<mainEngineControl> & mec, std::shared_ptr<doge::object> & obj);
+		virtual bool check(std::unique_ptr<mainEngineControl> & mec, const std::shared_ptr<doge::object> & obj) const;
+		void setMoveSpeed(float moveSpeed);
+		void setKeyBinding(int front,int back,int left,int right);
+	};
+
+	class lookAround : public action {	
+		virtual void execute(std::unique_ptr<mainEngineControl> & mec, std::shared_ptr<doge::object> & obj);
+		virtual bool check(std::unique_ptr<mainEngineControl> & mec, const std::shared_ptr<doge::object> & obj) const;
+	};
 };
