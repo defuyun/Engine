3d081fc003b4e26c0619b9a56a444a4038ba7745
defuyun <carlliu2012@gmail.com>
2019-01-09 18:21:29 +0800

    restart proj

--- Engine/shaders/square_texture_f.glsl
+++
@@ -1,9 +0,0 @@
-#version 430
-
-in vec2 texCoord;
-uniform sampler2D testTexture;
-out vec4 color;
-
-void main() {
-	color = texture(testTexture, texCoord);
-}
--- Engine/tests/mesh.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "../header.h"
-
-extern GLfloat indicesTriangle[36];
-extern GLshort indexesTriangle[6];
-extern GLfloat indicesSquareTexture[30];
-extern GLfloat indicesCombined[66];
-extern GLfloat indicesCube[288];
